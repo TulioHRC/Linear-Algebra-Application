1f56d36e83931a58f1bf697741832d2c8c9d6e93
TulioHRC <grove1411@gmail.com>
2024-01-26 22:00:02 -0300

    refactor: cleaning code

--- src/main.cpp
+++ src/main.cpp
@@ -4,6 +4,32 @@
 #include <iostream>
 #include <limits>
 
+void commandSwitch(std::string command, std::vector<Matrix *> &matrices)
+{
+    if (command == "c" || command == "create")
+    {
+        std::vector<std::vector<long long>> *table = getTable();
+        Matrix *matrix = new Matrix((*table).size(), (*table)[0].size(), *table);
+        matrices.push_back(matrix);
+
+        std::cin.get();
+    }
+    if (command == "s" || command == "show")
+    {
+        std::cout << "\n\tSaved Matrices:" << std::endl;
+        for (size_t i = 0; i < matrices.size(); i++)
+        {
+            std::cout << "\n(" << i + 1 << ")" << std::endl;
+            matrices[i]->showMatrix();
+        }
+    }
+    if (command == "e" || command == "exit")
+    {
+        std::cout << "Finishing program..." << std::endl;
+        exit(0);
+    }
+}
+
 int main()
 {
     std::cout << "Starting...\n"
@@ -17,28 +43,7 @@ int main()
     while (command != "q" && command != "quit")
     {
         std::cin.ignore();
-        if (command == "c" || command == "create")
-        {
-            std::vector<std::vector<long long>> *table = getTable();
-            Matrix *matrix = new Matrix((*table).size(), (*table)[0].size(), *table);
-            matrices.push_back(matrix);
-
-            std::cin.get();
-        }
-        if (command == "s" || command == "show")
-        {
-            std::cout << "\n\tSaved Matrices:" << std::endl;
-            for (size_t i = 0; i < matrices.size(); i++)
-            {
-                std::cout << "\n(" << i + 1 << ")" << std::endl;
-                matrices[i]->showMatrix();
-            }
-        }
-        if (command == "e" || command == "exit")
-        {
-            std::cout << "Finishing program..." << std::endl;
-            exit(0);
-        }
+        commandSwitch(command, matrices);
 
         std::cout << "\nPress enter to continue..." << std::endl;
         std::cin.ignore();
